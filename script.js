7fb5dd97a61051bfe5fab6bde510ff7d0d2e9cd3
varanchaleekh <bollrin800@gmail.com>
2021-05-25 01:40:12 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+// function myFuncyion(value1, value2){
+//     return value1 * value2;
+// }
+
+// console.log(myFuncyion(5, 5));
+
+function changeBg(){
+    document.body.style.backgroundColor = "red";
+}
+
+// changeBg();
+
+function cTof(celsios){
+    var cTemp = celsios;
+    var cTofar = cTemp * 9 /5 + 32;
+    var message = cTemp + "is" + cTofar;
+    console.log(message);
+}
+
+cTof(30);
