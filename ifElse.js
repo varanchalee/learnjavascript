5622ecf820bbb737cfd96d1d7619bcc6e55f0a5e
varanchaleekh <bollrin800@gmail.com>
2021-06-16 02:04:36 +0700

    if else

---
+++ ifElse.js
@@ -0,0 +1,25 @@
+//Syntax
+// if (condition) {
+//     //  in {} = do something
+// }
+
+// one condition
+// var time = 14;
+
+// if (time < 12) {
+//     console.log("Good Morning");
+// } else {
+//     console.log("Good Afternoon");
+// }
+
+// two condition
+
+var time = 20;
+
+if (time < 12) {
+    console.log("Good Morning");
+} else if(time < 18) {
+    console.log("Good Afternoon");
+} else {
+    console.log("Good evening");
+}
