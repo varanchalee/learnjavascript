bf056485ca090ce6b657c2ecf49f5f4229d876b7
varanchaleekh <bollrin800@gmail.com>
2021-06-16 01:42:53 +0700

    data type

---
+++ dataTypes.js
@@ -0,0 +1,13 @@
+// 7 Data Types in Javascripts
+
+// 
+//Primative Type
+var myNum = 10;
+var myString = "varanchalee";
+var myBoolean = true;
+var myBoolean2 = false;
+var myNull = null;
+var myundefined = undefined;
+
+//Objects Type
+var myObjects = {};
