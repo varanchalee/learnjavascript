37da355474bd2f3f0dd56f7ab8ca1d7c85d5e3af
varanchaleekh <bollrin800@gmail.com>
2021-06-16 01:48:04 +0700

    add typeof

--- dataTypes.js
+++ dataTypes.js
@@ -6,8 +6,10 @@ var myNum = 10;
 var myString = "varanchalee";
 var myBoolean = true;
 var myBoolean2 = false;
-var myNull = null;
+var myNull = null
 var myundefined = undefined;
 
 //Objects Type
 var myObjects = {};
+
+// typeof = เช็คชนิดและ data types JS
