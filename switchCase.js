c29d51aa25bfce9f4018f91164ffcbd4c429b961
varanchaleekh <bollrin800@gmail.com>
2021-06-20 15:52:18 +0700

    switch case

---
+++ switchCase.js
@@ -0,0 +1,12 @@
+myPhone = "sumsung";
+switch(myPhone){
+    case "sumsung":
+        console.log("18000");
+        break;
+    case "iphone":
+        console.log("20000");
+        break;
+    default:
+        console.log("don't have phone")
+
+}
