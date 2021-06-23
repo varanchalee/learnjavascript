eb4ce9ab5dd48e61efc1b3d8a7e37ab151f1bf93
varanchaleekh <bollrin800@gmail.com>
2021-06-23 16:35:25 +0700

    valiable let and const

---
+++ let&const.js
@@ -0,0 +1,15 @@
+// ES6 let & const
+var x =5;
+var x =6;
+console.log(x);
+// console = 6 
+
+let y = 6;
+    y = 7; // y = 7 คือการประกาศใหม่
+//let y = 7 ;// let y = 7; คือการประกาศซ้ำ = error
+console.log(y);
+
+const z = 10;
+const z = 11;
+console.log(z);
+// const = ค่าคงที่ ประกาศซ้ำและประกาศใหม่ไม่ได้
