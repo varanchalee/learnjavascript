447540958285dfccfe9c8408cca2e2240be8bf8c
varanchaleekh <bollrin800@gmail.com>
2021-06-23 17:32:44 +0700

    template string not use concetnate +" " +

---
+++ templateString.js
@@ -0,0 +1,18 @@
+// S t r i n g
+
+//keybord alt+9+6 is ``
+
+// var myName = "varanchalee";
+// console.log("Hi, my name is" +" " + myName + " " + "nice to meet you");
+//Hi, my name is varanchalee nice to meet you
+
+var myName = "varanchalee";
+console.log(`Hi, my name is ${myName} nice to meet you`);
+//Hi, my name is varanchalee nice to meet you
+
+//P a r a m i t o r  F u n c t i o n 
+function myName2 (name) {
+    console.log(`Hi, my name is  ${(name)}`);
+}
+
+myName2("varanchalee");
